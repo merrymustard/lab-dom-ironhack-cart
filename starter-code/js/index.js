f8ed504bef44a14d5a4b0b810f6922a6c95c127d
merrymustard <merryymustard@gmail.com>
2020-01-16 09:25:11 -0600

    update

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,31 +1,54 @@
 var $cart = document.querySelector("#cart tbody")
 var $calc = document.getElementById("calc")
-var $total = document.querySelector("h2 span")
-var $create = document.querySelector("#create")
-var $inputName = document.querySelector("#inputName")
-var $inputPrice = document.querySelector("#inputPrice")
-var totalSum
+var $delete = document.querySelector(".btn.btn-delete")
 
-//Get trbody node
-product = $cart.childNodes
-//tr list
-let productList = []
-//buttons list
-let removeBtnList = []
+let items = []
+$cart.querySelectorAll("tr").forEach($cart => {
+  items.push($cart)
+})
+let number = items.length
 
-console.log(productList, removeBtnList)
+function updateSubtot($product) {
+  // Iteration 1.1
+  let basePrice = $product.querySelector(".pu > span").innerHTML
+  let qty = $product.querySelector(".qty > label > input").value
+  let subTotal = basePrice * qty
+  $product.querySelector(".subtot > span").innerHTML = subTotal
+  return subTotal
+}
 
-$create.addEventListener("click", () => {
-  $cart.lastElementChild.insertAdjacentHTML(
-    "afterend",
-    `
-    <tr class="product">
+function calcAll() {
+  // Iteration 1.2
+  let total = 0
+  document.querySelectorAll(".product").forEach(function($product) {
+    total += updateSubtot($product)
+  })
+  document.querySelector("h2 > span").innerHTML = total
+}
+
+function deleteRow(index) {
+  const $item = items[index]
+  console.log($item)
+  const father = $item.parentNode
+  father.removeChild($item)
+}
+
+function createItem() {
+  let number = items.length
+  const $productName = document.querySelector('tfoot input[type = "text"]')
+  const $productPrice = document.querySelector('tfoot input[type = "number"]')
+  console.log(number)
+
+  const newItem = document.createElement("tr")
+  newItem.className = "product"
+
+  const $newRow = `
   <td class="name">
-    <span>${$inputName.value}</span>
+    <span>${$productName.value}</span>
   </td>
 
   <td class="pu">
-    $<span>${$inputPrice.value}</span>
+    $<span>${$productPrice.value}</span>
   </td>
 
   <td class="qty">
@@ -39,60 +62,15 @@ $create.addEventListener("click", () => {
   </td>
 
   <td class="rm">
-    <button class="btn btn-delete">Delete</button>
+    <button class="btn btn-delete" onclick="deleteRow(${number})">Delete</button>
   </td>
-</tr>`
-  )
-  productList.push($cart.lastElementChild)
-  let removeBtn = $cart.lastElementChild.children[4]
-  removeBtnList.push(
-    removeBtn.addEventListener("click", () => {
-      console.log(productList[productList.length - 1])
-      $cart.removeChild(productList[productList.length - 1])
-      productList.pop()
-      removeBtnList.pop()
-    })
-  )
-})
+`
+  newItem.innerHTML = $newRow
 
-function updateSubtot($product) {
-  // Iteration 1.1
-  //Getting pu and qty from tr children
-  return (
-    Number($product.children[1].children[0].innerText) *
-    Number($product.children[2].children[0].children[0].value)
-  )
+  $cart.appendChild(newItem)
+  items.push(newItem)
+  $productName.value = ""
+  $productPrice.value = ""
 }
-
-function calcAll() {
-  // Iteration 1.2
-  totalSum = 0
-  productList.forEach((el, idx) => {
-    console.log(productList[idx])
-    productList[idx].children[3].innerText = `$${updateSubtot(
-      productList[idx]
-    )}`
-    totalSum += updateSubtot(productList[idx])
-  })
-  $total.innerText = totalSum
-  console.log($cart, productList, removeBtnList, product)
-}
-
-//get tr list and button list
-product.forEach((el, idx) => {
-  if (idx % 2 != 0) {
-    productList.push(el)
-    removeBtnList.push(el.children[4])
-  }
-})
-removeBtnList.forEach((el, idx) => {
-  el.addEventListener("click", e => {
-    $cart.removeChild(productList[idx])
-    productList.pop()
-    removeBtnList.pop()
-  })
-})
 
 $calc.onclick = calcAll
-
-console.log($cart, productList, removeBtnList, product)
